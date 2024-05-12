684524da44d9b060b4c2451293b961ed718fb603
ivayanc <ivanyanchenko.odb@gmail.com>
2024-05-12 07:04:07 +0300

    Update dockerfile/initial script

--- backend/utils/initial_scripts.py
+++ backend/utils/initial_scripts.py
@@ -15,7 +15,31 @@ def create_basic_regions() -> None:
             latitude=48.73755455924443,
             longitude=37.583940377427
         )
+        region3 = Region(
+            region_name="Слов'янськ",
+            latitude=48.853952541872445,
+            longitude=37.60345220052644
+        )
+        region4 = Region(
+            region_name="Часів Яр",
+            latitude=48.58862829983634,
+            longitude=37.83539730105749
+        )
+        region5 = Region(
+            region_name="Торецьк",
+            latitude=48.397372179321536,
+            longitude=37.85516056686654
+        )
+        region6 = Region(
+            region_name="Нью-Йорк",
+            latitude=48.33358116771939,
+            longitude=37.8400966471821
+        )
         s.add(region)
         s.add(region2)
+        s.add(region3)
+        s.add(region4)
+        s.add(region5)
+        s.add(region6)
         s.commit()
         s.refresh(region)
